96d6acbc5395d7340569c8981a7ccfecc8fef168
Aymen Mohammed <aymimanmoh8@gmail>
2025-12-25 01:29:06 +0300

    database configuration api added

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,21 +1,24 @@
 
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth'; // Auth
-import { getFirestore } from 'firebase/firestore'; // Firestore
 
-// TODO: Replace with your actual Firebase config keys
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyClBUnyEvOyW45Tkb4jUJPr82ANHCL2_Wc",
+    authDomain: "food-delivery-1827f.firebaseapp.com",
+    projectId: "food-delivery-1827f",
+    storageBucket: "food-delivery-1827f.firebasestorage.app",
+    messagingSenderId: "659249963888",
+    appId: "1:659249963888:web:a5efce7ffcef65e7b925b5",
+    measurementId: "G-TT8YX34H37"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Initialize Auth and Firestore
-export const auth = getAuth(app);
-export const db = getFirestore(app);
