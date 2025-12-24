84293d8465558b8212f4b3dce2e5a06b2b1b1a80
Aymen Mohammed <aymimanmoh8@gmail>
2025-12-25 01:43:18 +0300

    fire base connected succesfully

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,13 +1,11 @@
 
-
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyClBUnyEvOyW45Tkb4jUJPr82ANHCL2_Wc",
     authDomain: "food-delivery-1827f.firebaseapp.com",
@@ -21,4 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Auth and Firestore and export them
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
